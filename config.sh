f3b1018f7e262dbde9241993eb71b8f425ad9433
Shahin Mohammadkhani <skhani@creativechannel.com>
2013-01-21 20:03:14 -0800

    update the rcs-keywords project. deprecated the clean and smudge as it was not the way we wanted to monitor our code. Also remove the all gitattribute stufff. created teh pre-commit hook and update the config.sh to move it automatically if someone wants to use it.

--- config.sh
+++ config.sh
@@ -1,40 +1,9 @@
 #!/bin/bash
 
-#if found text dont do anything, else insert it into the file
-# exit status is 0 (true) if not found and 1 (false) if found
-if grep -Lq "rcs-keywords" ~/.gitconfig
-then
-    echo "rcs-keywords already exist in gitconfig. Moving on";
-else
-    echo '
-[filter "rcs-keywords"]
-    clean  = .git_filters/rcs-keywords.clean
-    smudge = .git_filters/rcs-keywords.smudge %f' >> ~/.gitconfig;
-fi
+#because of security the pre-commit hook can not be instantiated by default. this script needs to be
+#ran for every project
 
-#execute the filters on php files
-#.gitattributes 
-#Map file extenstions to git filters
+file="$1".git/hooks/pre-commit;
 
-DIRECTORY=$1;
-FILE="$1".gitattributes;
-
-# if file does not exist create it and put the filters in it.
-# elseif the keywords already there let the user know.
-# else(the file exist bu the keywords are not) insert the keywords in there.
-# the grep condition status results to 0 if true , and 1 if false;
-if [ ! -f "$FILE" ] 
-then
-	touch  "$DIRECTORY".gitattributes;
-	echo '*.php filter=rcs-keywords' >> "$DIRECTORY".gitattributes;
-
-elif grep -Lq "*.php filter=rcs-keywords" "$FILE" 
-then
-	echo "rcs-keywords filter for php files already exist in .gitattributes. Moving on";
-
-else
-	echo '*.php filter=rcs-keywords' >> "$DIRECTORY".gitattributes;
-fi
-
-#copy the filter actions to the project directory
-cp -r .git_filters "$DIRECTORY";
+#copy the pre-commit hook to the hooks folder 
+cp -r pre-commit "$file";
