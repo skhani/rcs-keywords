0d5b443ae067b43d8293f6d55c7cc70dc8ef95c0
Shahin Mohammadkhani <skhani@creativechannel.com>
2013-01-22 11:51:39 -0800

    fixes #9 - added case statement to check for directory input and add trailing slash if necessary

--- config.sh
+++ config.sh
@@ -3,7 +3,18 @@
 #because of security the pre-commit hook can not be instantiated by default. this script needs to be
 #ran for every project
 
-file="$1".git/hooks/pre-commit;
+directory="$1"
+
+#case statement to check if the input directory has a backslash or not
+#then appending the pre-commit file
+case "$directory" in 
+    */)
+        file="$1".git/hooks/pre-commit;
+        ;;
+    *)
+        file="$1"/.git/hooks/pre-commit;
+        ;;
+esac
 
 #copy the pre-commit hook to the hooks folder 
 cp -r pre-commit "$file";
