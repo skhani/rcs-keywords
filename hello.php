f7715c3961849858739b23f65c25093c9b018bb1
Shahin Mohammadkhani <skhani@creativechannel.com>
2013-03-19 08:40:03 -0700

    her

--- hello.php
+++ hello.php
@@ -1,7 +1,7 @@
 <?php
 /**
- *  %Author: Shahin Mohammadkhani %
- *  %Date: 03/19/2013 08:29:16 AM %
+ *  %Author: Shahin Mohammadkhani skhani@creativechannel.com %
+ *  %Date: 03/19/2013 08:29:27 AM %
  *  $Revision$
  *  $Id$
  **/
